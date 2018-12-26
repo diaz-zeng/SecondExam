ae6af104ccf14f40a72c972fad7ef7109790cf71
Diaz <zly981026@outlook.com>
2018-12-27 00:12:09 +0800

    201812170012

--- src/com/diaz/secondexam/controller/UserActionServlet.java
+++ src/com/diaz/secondexam/controller/UserActionServlet.java
@@ -4,6 +4,7 @@ import com.diaz.secondexam.proxy.ServiceProxy;
 import com.diaz.secondexam.service.UserService;
 import com.diaz.secondexam.service.UserServiceImpl;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
@@ -13,6 +14,7 @@ import java.util.List;
  * @author Diaz
  * @since 2018/12/26 20:00
  */
+@WebServlet("/userAction")
 public class UserActionServlet extends AjaxServlet {
     private UserService userService = (UserService) ServiceProxy.getProxyInstance(UserServiceImpl.class);
 
@@ -20,17 +22,15 @@ public class UserActionServlet extends AjaxServlet {
         try {
             String[] sid = request.getParameterValues("id");
             Integer[] id = new Integer[sid.length];
-            StringBuilder results = new StringBuilder();
             PrintWriter writer = response.getWriter();
             for (int i = 0; i < sid.length; i++) {
                 id[i] = Integer.parseInt(sid[i]);
                 List<Integer> failed = userService.removeUser(id);
                 if (failed.size() > 0) {
-                    results.append("下列用户删除失败，可能不存在:");
-                    for (Integer j : failed) {
-                        results.append(j.toString() + ";");
-                    }
-                    writer.write(results.toString());
+                    writer.write("用户删除失败，可能不存在:");
+                    writer.flush();
+                } else {
+                    writer.write("用户删除成功:");
                     writer.flush();
                 }
             }
--- web/js/index.js
+++ web/js/index.js
@@ -1,12 +1,39 @@
 $(function () {
     $("#selectAll").onclick(function () {
-        if(this.checked){
-            $("[name=id]:checkbox").prop("checked",true);
-        }else{
-            $("[name=id]:checkbox").prop("checked",true);
+        if (this.checked) {
+            $("[name=id]:checkbox").prop("checked", true);
+        } else {
+            $("[name=id]:checkbox").prop("checked", true);
         }
     })
-    $("#indexForm").onsubmit(function () {
-
-    })
 })
+
+function onFormSubmit() {
+    // if($("#indexForm").serializeArray().length>0){
+    //
+    // }
+    var checkedID = [];
+    $("[name=id]:checkbox").each(function (i) {
+        if(this.checked){
+            checkedID[i] = this.val;
+        }
+    })
+    if(checkedID.length>0){
+        $.ajax({
+            type: "POST",
+            dataType: "text",
+            url: "/userAction?action=removeUser",
+            async: true,
+            data: {id:checkedID},
+            success: function (result) {
+                alert(result.responseText);
+            },
+            error: function () {
+                alert("操作失败");
+            }
+        })
+    }else {
+        alert("请最少选择一个用户");
+    }
+
+}
